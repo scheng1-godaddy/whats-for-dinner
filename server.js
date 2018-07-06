ad85344ea903f9484d47f257e8b05017f58c0c97
shawnc8160 <shawnc8160@gmail.com>
2018-07-06 07:45:28 -0700

    Started express, created basic route

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('this works');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This works')
+})
+
+app.listen(3000, () => {
+  console.log('listening');
+})
