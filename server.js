4258bd909d007fc08955c0e4098a4be06ace7bca
shawnc8160 <shawnc8160@gmail.com>
2018-07-06 08:52:00 -0700

    setup mongo and mongoose

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/project2';
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('This works')
@@ -8,4 +10,9 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log('listening');
+})
+
+mongoose.connect(mongoUri);
+mongoose.connection.on('open', () => {
+  console.log('Connected to mongoose');
 })
